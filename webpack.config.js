04be9e778ae1d624a963561e4811295a629ba908
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-07-20 18:38:26 -0700

    add .babelrc configs to project

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'eval-source-map', 
-  devServer: {                 
-    contentBase: './dist'
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
   },
   plugins: [
     new CleanWebpackPlugin(),
